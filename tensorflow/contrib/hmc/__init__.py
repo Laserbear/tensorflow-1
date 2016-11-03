09a3edbf17fee1c50c298781d79392b4f22a1d25
Christian Ng <christianr.ng@gmail.com>
2016-11-03 16:13:47 -0700

    add files whoops

---
+++ tensorflow/contrib/hmc/__init__.py
@@ -0,0 +1,10 @@
+"""hmc.py
+## This package provides classes for Hamiltonian Monte Carlo
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# pylint: disable=unused-import,wildcard-import, line-too-long
+from tensorflow.contrib.hmc.python.ops.hmc import *
